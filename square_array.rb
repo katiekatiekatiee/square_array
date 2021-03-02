53634afe4be1d0c5be49ce4faf17eba6c8b0fdd5
Catherine Hodgkinson <catherinemhodgkinson@gmail.com>
2021-03-01 23:38:21 -0500

    commit tree edcb1f3e7372111e65f341147d64a77e7910d468

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,16 @@
+
+
+#def square_array(array)
+  #squared_array = []
+  #array.each do |number|
+#     squared_numbers = number ** 2
+#     squared_array << squared_numbers
+#    end
+#   squared_array
+# end
+
 def square_array(array)
-  # your code here
+  squared_array = []
+  array.each {|number| squared_array << number ** 2}
+  squared_array
 end
